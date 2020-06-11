04b885728b5d375e71682c7244b73ad8b1197d3b
Spiritusanti <mccrackenjacob@gmail.com>
2020-06-11 11:20:45 -0700

    minor change temporarily removing database

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res)=> {
-    res.send(database.users);
+    res.send('this is working!');
 })
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
